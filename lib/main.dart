a0fb1b623d3cd9be79dde1c45fef1044c0ceaa5c
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-11 18:23:50 +0530

    FLUT-3516 - Sample and screenshot added.

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(NavigationArrow());
+
+class NavigationArrow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Card(
+          margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+          child: SfDateRangePicker(
+            view: DateRangePickerView.month,
+            showNavigationArrow: true,
+            headerStyle:
+                DateRangePickerHeaderStyle(textAlign: TextAlign.center),
+          ),
+        )));
+  }
+}
